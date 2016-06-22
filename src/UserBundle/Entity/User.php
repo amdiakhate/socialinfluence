0bda2f2ef8defef8382c7a7b60ef2f45e1519672
amdiakhate <maxtar.diakhate@gmail.com>
2016-06-22 21:04:57 +0200

    initial

---
+++ src/UserBundle/Entity/User.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: maxta
+ * Date: 22/06/2016
+ * Time: 20:58
+ */
+namespace UserBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+}
